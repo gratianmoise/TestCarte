92b1002a103317c47735825867ecca301c675306
gratianmoise <gratianmoise@gmail.com>
2021-01-31 20:41:18 +0200

    modificare an

--- src/test/java/CarteTest.java
+++ src/test/java/CarteTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 public class CarteTest {
     @Test
     void verificVechime(){
-        Carte p1 = new Carte("A", "B", "C", 2018);
+        Carte p1 = new Carte("A", "B", "C", 2014);
         int vechime = p1.vechimeCarte(p1);
-        Assertions.assertEquals(vechime, 3);
+        Assertions.assertEquals(vechime, 7);
         Assertions.assertNotEquals(vechime, 2);
         //Assertions.assertNotEquals(vechime, 3);
+
     }
 }
